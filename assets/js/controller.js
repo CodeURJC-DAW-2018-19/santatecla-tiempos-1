158a45c27e2716d6883bccc50ee16961f2e73ea1
MariaDelgadoEiriz2 <m.delgadoei@alumnos.urjc.es>
2019-02-04 12:32:31 +0100

    Popup de ejemplo en la pestaña intervalos.

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -47,20 +47,3 @@ function goTo(route){
     document.getElementById("nav_"+route).className = "nav_item active"   
 }
 
-
-
-var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
-	overlay = document.getElementById('overlay'),
-	popup = document.getElementById('popup'),
-	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
-
-btnAbrirPopup.addEventListener('click', function(){
-	overlay.classList.add('active');
-	popup.classList.add('active');
-});
-
-btnCerrarPopup.addEventListener('click', function(e){
-	e.preventDefault();
-	overlay.classList.remove('active');
-	popup.classList.remove('active');
-});
