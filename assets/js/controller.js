d2a92f6f86b8ca098ed4bdc33297ea2e5f328afe
MariaDelgadoEiriz2 <m.delgadoei@alumnos.urjc.es>
2019-02-04 11:25:21 +0100

    Estructura interior de cada apartado

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -44,7 +44,23 @@ function goTo(route){
     document.getElementById("nav_category").className = "nav_item"
     document.getElementById("nav_time").className = "nav_item"
     document.getElementById("nav_event").className = "nav_item"
-    document.getElementById("nav_"+route).className = "nav_item active"
-    
-    
+    document.getElementById("nav_"+route).className = "nav_item active"   
 }
+
+
+
+var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+
+btnAbrirPopup.addEventListener('click', function(){
+	overlay.classList.add('active');
+	popup.classList.add('active');
+});
+
+btnCerrarPopup.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
+});
