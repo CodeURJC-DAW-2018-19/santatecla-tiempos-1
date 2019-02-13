255ea016b20145c0ed9aebee8056d2526784444f
MiguelTG35 <migueltg352340@gmail.com>
2019-02-13 15:29:07 +0100

    Created Spring Boot Project
    Adding spring boot project.

---
+++ santatecla/src/main/java/com/dawsantateclatiempos1/santatecla/SantateclaApplication.java
@@ -0,0 +1,14 @@
+package com.dawsantateclatiempos1.santatecla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantateclaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantateclaApplication.class, args);
+	}
+
+}
+
---
+++ santatecla/src/test/java/com/dawsantateclatiempos1/santatecla/SantateclaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dawsantateclatiempos1.santatecla;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SantateclaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
